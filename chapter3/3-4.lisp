bd4e2c9ccf986f5a41f89bfcf74534a44edcd02a
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2021-02-22 23:49:36 +0100

    3-4

--- chapter3/3-4.lisp
+++ chapter3/3-4.lisp
@@ -1,22 +1,32 @@
 ;; Write a function that, like the regular print function, will print an expression in dotted pair notation when necessary but will use normal list notation when possible.
 
+(Setf x 100)
 
+(defun my-print1 (Lst)
+  (when (> 0 (decf x))
+    (return my-print1))
 
-(defun my-print (lst)
-  (when (listp (first lst))
-      (princ "(")
-      (my-print (first lst))
-      (print ")"))
-  (when (atom (first lst))
-    (princ (first lst))
-    (my-print (rest lst)))
-  (when (and (consp lst)
-             (not (consp (rest lst))))
-    (princ ".")
-    (my-print (rest lst))
-    )
-  )
+  (cond ((null lst)
+         (princ "nil"))
+        ((atom lst)
+         (princ lst))
+        ((atom (first lst))
+         (princ (first lst))
+         (princ " ")
+         (print-rest (rest lst)))
+        ((listp (first lst))
+         (princ "(")
+         (my-print1 (first lst))
+         (princ ") ")
+         (print-rest (rest lst)))
+        ))
 
+(defun print-rest (lst)
+  (cond
+    ((atom lst)
+     (princ " . "))
+    (t
+     (princ " ")))
+  (my-print1 lst))
 
-
-(my-print '(1 2 3 . ( 5 5 .7)))
+(my-print1 '(1 30 (t . b) (2 3) 3 (8 10 . 9) . ( 5 5 .7)))
