945c2bdf6e517f1cd2eaa5483549b132c7172876
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2021-03-21 22:21:21 +0100

    no more: goal is accidentally solved as part of other goal

--- chapter4/4-3.lisp
+++ chapter4/4-3.lisp
@@ -22,3 +22,58 @@
 
 ;; (2)
 (gps nil '((executing (eat dessert))))
+#+ nil ((START)
+        (EXECUTING (GET CAKE FROM BAKERY))
+        (EXECUTING (EAT DESSERT))
+        (EXECUTING (EAT DESSERT)))
+
+;; because this seems a little bit bugged, i made second mission where the goal is to get stuffed
+
+;; (1)
+
+(use (list
+      (op '(eat ice cream)
+          :add-list '((stuffed))
+          :preconds '((ice cream))
+          :del-list '((ice cream)))
+      (op '(buy cake)
+          :add-list '((cake)))
+      (op '(eat cake)
+          :add-list '((stuffed) (ice cream))
+          :preconds '((cake))
+          :del-list '((cake)))
+      ))
+
+;; when first is swaped with last, the problem is 'fixed'
+
+;; (2)
+(gps nil '((stuffed)))
+
+#+nil ((Start)
+       (EXECUTING (BUY CAKE))
+       (EXECUTING (EAT CAKE))
+       (EXECUTING (EAT ICE CREAM)))
+
+
+(defun achievedp (state goal)
+  (member-equal goal state))
+
+(defun apply-op (state goal op goal-stack)
+  "Return a new, transformed state if op is applicable."
+  (dbg-indent :gps (length goal-stack) "Consider: ~a" (op-action op))
+  (let ((state2 (achieve-all state
+                             (op-preconds op)
+                             (cons goal goal-stack))))
+    (when state2
+      ;; Return an updated state
+      (if (achievedp state2 goal)
+          (progn
+            (dbg-indent :gps (length goal-stack) "It seeams that goal (~a) is already achieved" goal)
+            state2)
+          (progn
+            (dbg-indent :gps (length goal-stack) "Action: ~a" (op-action op))
+            (append (remove-if #'(lambda (x)
+                                   (member-equal x (op-del-list op)))
+                               state2)
+                    (op-add-list op)))
+          ))))
