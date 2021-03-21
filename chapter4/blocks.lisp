b8249d215f1f2de38c20a2cf43d724bc12396f20
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2021-03-21 22:20:25 +0100

    old code from blocks

--- chapter4/blocks.lisp
+++ chapter4/blocks.lisp
@@ -1,16 +1,42 @@
 (load "./gps2.lisp")
 
+(defun achieve (state goal goal-stack)
+  "A goal is achieved if it already holds,
+  or if there is an appropriate op for it that is applicable."
+  (dbg-indent :gps (length goal-stack) "Goal: ~a" goal)
+  
+  (cond ((member-equal goal state) state)
+        ((member-equal goal goal-stack) nil)
+        (t (some #'(lambda (op) (apply-op state goal op goal-stack))
+                 (appropriate-ops goal state))))) ;***
+
+(defun appropriate-ops (goal state)
+  "Return a list of appropriate operators,
+  sorted by the number of unfulfilled preconditions."
+  (sort (copy-list (find-all goal *ops* :test #'appropriate-p))
+        #'<
+        :key #'(lambda (op)
+                 (count-if #'(lambda (precond)
+                               (not (member-equal precond state)))
+                           (op-preconds op)))))
 
 (defun achieve-all (state goals goal-stack)
-  (some #'(lambda (goals)
-            (achieve-each state goals goal-stack))
-        (orderings goals)))
+  "Achieve each goal, trying several orderings."
 
-(defun achieve-each (State goals goal-stack)
+ 
+  (let ((a (if nil
+               state
+               (some #'(lambda (goals) (achieve-each state goals goal-stack))
+                     (orderings goals)))))
+    ;;(dbg-indent :gps (length goal-stack) "XXX: ~a" a)
+    a))
+
+(defun achieve-each (state goals goal-stack)
+  "Achieve each goal, and make sure they still hold at the end."
   (let ((current-state state))
-    (if (and (every (lambda (g)
-                      (setf current-state
-                            (achieve current-state g goal-stack)))
+    (if (and (every #'(lambda (g)
+                        (setf current-state
+                              (achieve current-state g goal-stack)))
                     goals)
              (subsetp goals current-state :test #'equal))
         current-state)))
@@ -20,30 +46,26 @@
       (list l (reverse l))
       (list l)))
 
-(defun move-ons (a b c)
-  (if (eq b 'table)
-      `((,a on ,c))
-      `((,a on ,c) (space))))
-
-(defun move-op (a b c)
-  (op `(move ,a from ,b to ,c)
-      :preconds `((space on ,a)
-                  (space on ,b)
-                  (,a on ,b))
-      :add-list (move-ons a b c)
-      :del-list (move-ons a c b)))
-
 (defun make-block-ops (blocks)
   (let ((ops nil))
     (dolist (a blocks)
       (dolist (b blocks)
         (unless (equal a b)
           (dolist (c blocks)
-            (unless (or (equal c a)
-                        (equal c b))
-              (push (move-op a b c)
-                    ops)))
-          (push (move-op a 'table b)
-                ops)
-          (push (move-op a b 'table)
-                ops))))))
+            (unless (or (equal c a) (equal c b))
+              (push (move-op a b c) ops)))
+          (push (move-op a 'table b) ops)
+          (push (move-op a b 'table) ops))))
+    ops))
+(defun move-op (a b c)
+  "Make an operator to move A from B to C."
+  (op
+   `(move ,a from ,b to ,c)
+   :preconds `((space on ,a) (space on ,c) (,a on ,b))
+   :add-list (move-ons a b c)
+   :del-list (move-ons a c b)))
+
+(defun move-ons (a b c)
+  (if (eq b 'table)
+      `((,a on ,c))
+      `((,a on ,c) (space on ,b))))
